d01b8e14156dfa7f809f6f0156aa7f7d422035e5
Drew Wyatt <drew.j.wyatt@gmail.com>
2018-08-21 00:27:56 -0400

    prettier updates + added module directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -14,21 +14,20 @@ module.exports = {
       {
         test: /\.ts$/,
         use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts']
+    extensions: ['.ts'],
+    modules: ['node_modules', 'src'],
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-    new CopyWebpackPlugin([
-      { from: './public' },
-    ], { flatten: true }),
+    new CopyWebpackPlugin([{ from: './public' }], { flatten: true }),
     new CleanWebpackPlugin(['dist']),
   ],
-};
+}
